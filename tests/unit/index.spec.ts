b33413f8a7f514ca2d5e5b22a9f38976a5a05f95
VSymonenko <simonenko744@gmail.com>
2020-06-07 13:52:37 +0300

    feat: add todos

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -2,6 +2,54 @@
 
 test.todo('should be lock');
 
-// test.todo('should be anchor');
+test.todo('should be anchor');
 
-// test
+test.todo('should be slide effect');
+
+test.todo('should be fade effect');
+
+test.todo('should be lazy load image');
+
+test.todo('should be support should touch');
+
+test.todo('should be configure sensitivity');
+
+test.todo('should be configure active class');
+
+test.todo('should be full height');
+
+test.todo('should be full width');
+
+test.todo('width must be 337');
+
+test.todo('height must be 443');
+
+test.todo('should be infinity loop');
+
+test.todo('should mobie');
+
+test.todo('should be next preview');
+
+test.todo('should be previous preview');
+
+test.todo('navigation must be invisible');
+
+test.todo('next preview should be third');
+
+test.todo('pagination must be invisible');
+
+test.todo('should be 3 slides per view');
+
+test.todo('space beetween slides must be 30px');
+
+test.todo('animation speed must be 553');
+
+test.todo('start index must be 2');
+
+test.todo('should be support breakpoints system');
+
+test.todo('should be support slots');
+
+test.todo('the data must be updated');
+
+test.todo('support resize window');
