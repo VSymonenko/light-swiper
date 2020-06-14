f16ff3b153508a85fd46540945814ac72e4068c6
VSymonenko <simonenko744@gmail.com>
2020-06-14 21:53:05 +0300

    feat: add options fixtures

---
+++ src/fixtures/index.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/prefer-default-export
+export const options = {
+  lock: false,
+  anchor: '',
+  effect: 'slide',
+  sensitivity: 1,
+  activeClass: '',
+  fullWidth: false,
+  fullHeight: false,
+  width: 100,
+  height: 100,
+  loop: false,
+  isMobile: false,
+  navigation: {
+    icons: {
+      next: '>',
+      previous: '<',
+    },
+    calibration: 15,
+  },
+  pagination: {
+    activeClass: 'active-page',
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetweenSlides: 15,
+  speed: 300,
+};
